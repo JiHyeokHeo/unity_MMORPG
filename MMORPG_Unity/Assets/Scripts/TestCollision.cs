510ad4e4a24f23df681d1b862d0faeb1edda3bc5
JiHyeok Heo <vndj1212@gmail.com>
2023-10-07 06:01:32 +0900

    RayCast의 다양한 응용 ( 카메라, 플레이어 활용 )

--- MMORPG_Unity/Assets/Scripts/TestCollision.cs
+++ MMORPG_Unity/Assets/Scripts/TestCollision.cs
@@ -28,6 +28,20 @@ public class TestCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+
+        foreach(RaycastHit hit in hits)
+        {
+            Debug.Log($"RayCast! {hit.collider.gameObject.name}");
+        }
+
+        //if(Physics.Raycast(transform.position + Vector3.up, look, out hit, 10))
+        //{
+        //    Debug.Log($"RayCast! {hit.collider.gameObject.name}");
+        //}
     }
 }
