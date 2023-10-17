12d21161b2caf097634fe977449038151e466aae
JiHyeok Heo <vndj1212@gmail.com>
2023-10-18 00:00:47 +0900

    MiniRPG

---
+++ MMORPG_Unity/Assets/Scripts/Contents/Stat.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    [SerializeField]
+    protected int _level;
+    [SerializeField]
+    protected int _hp;
+    [SerializeField]
+    protected int _maxHp;
+    [SerializeField]
+    protected int _attack;
+    [SerializeField]
+    protected int _defence;
+    [SerializeField]
+    protected float _speed;
+
+    public int Level { get { return _level; } set { _level = value; } }
+    public int Hp { get { return _level; } set { _hp = value; } }
+    public int MaxHp { get { return _level; } set { _maxHp = value; } }
+    public int Attack { get { return _level; } set { _attack = value; } }
+    public int Defence { get { return _level; } set { _defence = value; } }
+    public float MoveSpeed { get { return _level; } set { _speed = value; } }
+
+    private void Start()
+    {
+        _level = 1;
+        _hp = 100;
+        _maxHp = 100;
+        _attack = 10;
+        _defence = 5;
+        _speed = 5.0f;
+    }
+}
--- MMORPG_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ MMORPG_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,21 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float _speed = 10.0f;
-
+    PlayerStat _stat;
     Vector3 _destPos;
-    
    
     void Start()
     {
+        _stat = gameObject.GetComponent<PlayerStat>();
+   
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
-
-
     }
 
     public enum PlayerState
@@ -23,6 +21,7 @@ public class PlayerController : MonoBehaviour
         Die,
         Moving,
         Idle,
+        Skill,
     }
 
     PlayerState _state = PlayerState.Idle;
@@ -35,21 +34,31 @@ public class PlayerController : MonoBehaviour
     void UpdateMoving()
     {
         Vector3 dir = _destPos - transform.position;
-        if (dir.magnitude < 0.0001f)
+        if (dir.magnitude < 0.1f)
         {
             _state = PlayerState.Idle;
         }
         else
         {
-            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-            transform.position += dir.normalized * moveDist;
+            NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
+            float moveDist = Mathf.Clamp(_stat.MoveSpeed * Time.deltaTime, 0, dir.magnitude);
+            nma.Move(dir.normalized * moveDist);
+
+            Debug.DrawRay(transform.position, dir.normalized, Color.green);
+            if(Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+            {
+                _state = PlayerState.Idle;
+                return;
+            }
+                
+            //transform.position += dir.normalized * moveDist;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
         }
 
         // 局聪皋捞记 贸府
         Animator ani = GetComponent<Animator>();
 
-        ani.SetFloat("speed", _speed);
+        ani.SetFloat("speed", _stat.MoveSpeed);
     }
 
     void OnRunEvent()
@@ -80,22 +89,29 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    int _mask =  (1 << (int)Define.Layer.Ground ) | (1 << (int) Define.Layer.Monster);
     void OnMouseClicked(Define.MouseEvent evt)
     {
         if (_state == PlayerState.Die)
             return;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+        //Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        if (Physics.Raycast(ray, out hit, 100.0f, _mask))
         {
             _destPos = hit.point;
             _state = PlayerState.Moving;
-            //Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
-        }
 
-        //Debug.Log("OnMouseClicked");
+            if(hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+                Debug.Log("Monster Click!");
+            }
+            else
+            {
+                Debug.Log("Ground Click!");
+            }
+        }
     }
 }
